3691003103cf2d5c93a817b173bea57311cc1d0f
lamuyen0214-byte <lamuyen.02.14@gmail.com>
2026-07-13 00:05:33 +0700

    Update teacher_support.py

--- views/teacher_support.py
+++ views/teacher_support.py
@@ -4,6 +4,7 @@ from modules.danh_cho_giao_vien.de_kt.de_kt_builder import render_de_kt_module
 # Khai báo sẵn module STEM để thầy ghép vào sau
 from modules.danh_cho_giao_vien.stem.stem_builder import render_stem_module
 from modules.danh_cho_giao_vien.rubric.rubric_builder import render_rubric_module
+from modules.danh_cho_giao_vien.organization.homeroom_builder import render_homeroom_module
 def render_module():
     st.markdown("## 👨‍🏫 Phân hệ: Hỗ trợ Giáo viên")
     
