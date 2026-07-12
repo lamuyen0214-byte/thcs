f81cd5a0eb30c0ceee4e0bd9710fd61c404cab43
lamuyen0214-byte <lamuyen.02.14@gmail.com>
2026-07-12 21:09:29 +0700

    Update department_mgmt.py

--- views/department_mgmt.py
+++ views/department_mgmt.py
@@ -1,1 +1,6 @@
+import streamlit as st  # Dòng này là bắt buộc để file này hiểu 'st' là gì
 
+def render_module():
+    # Toàn bộ code chức năng của thầy ở đây
+    st.subheader("Hỗ trợ Giáo viên")
+    # ...
