4962101999dd491f996f38818fa40a916d450947
lamuyen0214-byte <lamuyen.02.14@gmail.com>
2026-07-13 14:41:02 +0700

    Update teacher_support.py

--- views/teacher_support.py
+++ views/teacher_support.py
@@ -9,13 +9,13 @@ if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
 # --- 2. IMPORT CÁC MODULE CHỨC NĂNG ---
-from modules.danh_cho_giao_vien.khbd.khbd_builder import render_khbd_module
-from modules.danh_cho_giao_vien.de_kt.de_kt_builder import render_de_kt_module
-from modules.danh_cho_giao_vien.stem.stem_builder import render_stem_module
-from modules.danh_cho_giao_vien.rubric.rubric_builder import render_rubric_module
-from modules.danh_cho_giao_vien.organization.homeroom_builder import render_homeroom_module
-from modules.danh_cho_giao_vien.quan_ly_diem.quan_ly_diem_builder import render_quan_ly_diem_module
-from modules.danh_cho_giao_vien.tao_prompt.prompt_builder import render_prompt_module
+#from modules.danh_cho_giao_vien.khbd.khbd_builder import render_khbd_module
+#from modules.danh_cho_giao_vien.de_kt.de_kt_builder import render_de_kt_module
+#from modules.danh_cho_giao_vien.stem.stem_builder import render_stem_module
+#from modules.danh_cho_giao_vien.rubric.rubric_builder import render_rubric_module
+#from modules.danh_cho_giao_vien.organization.homeroom_builder import render_homeroom_module
+#from modules.danh_cho_giao_vien.quan_ly_diem.quan_ly_diem_builder import render_quan_ly_diem_module
+#from modules.danh_cho_giao_vien.tao_prompt.prompt_builder import render_prompt_module
 
 # --- 3. HẰNG SỐ GIAO DIỆN (DRY - DON'T REPEAT YOURSELF) ---
 WELCOME_MESSAGE = "⏳ Chào mừng quý thầy cô đến với nền tảng số AI tích hợp chuyên sâu, trường THCS Nguyễn Chí Thanh, P.Tân Lập, tỉnh Đắk Lắk."
