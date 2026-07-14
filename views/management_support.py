bcb329dbcb6d1cf9678a27521547f856ba6f37bf
lamuyen0214-byte <lamuyen.02.14@gmail.com>
2026-07-14 14:05:07 +0700

    Update management_support.py

--- views/management_support.py
+++ views/management_support.py
@@ -2,6 +2,5 @@ import streamlit as st
 from modules.management.org_manager import render_org_management
 
 def render_management_view():
-    st.header("📊 Phân hệ: Quản lý Tổ chuyên môn")
-    # Gọi hàm render đã thiết kế ở Bước 1
+    # Không cần header thừa nếu tab đã rõ ràng
     render_org_management()
