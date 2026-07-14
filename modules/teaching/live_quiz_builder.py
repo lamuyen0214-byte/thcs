57f8749ca161735d486d37ce8f09fe6ae64ce163
lamuyen0214-byte <lamuyen.02.14@gmail.com>
2026-07-14 11:56:26 +0700

    Update live_quiz_builder.py

--- modules/teaching/live_quiz_builder.py
+++ modules/teaching/live_quiz_builder.py
@@ -1,6 +1,6 @@
 import os
 # Đoạn code kiểm tra đường dẫn
-st.write("Đường dẫn hiện tại:", os.getcwd())
+st.write("https://drive.google.com/file/d/1j1FzoOvKWPsrSlEkBMdObiiKJFmkS5RM/view?usp=sharing", os.getcwd())
 st.write("Danh sách file trong thư mục assets:", os.listdir('assets') if os.path.exists('assets') else "Không tìm thấy thư mục assets!")
 import streamlit as st
 import time
