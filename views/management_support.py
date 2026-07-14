3d2f055f7646e1aa99e8e87f47a92edd80def787
lamuyen0214-byte <lamuyen.02.14@gmail.com>
2026-07-14 14:14:55 +0700

    Update management_support.py

--- views/management_support.py
+++ views/management_support.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from modules.management.org_manager import render_org_management
-
 def render_management_view():
     # Không cần header thừa nếu tab đã rõ ràng
     render_org_management()
